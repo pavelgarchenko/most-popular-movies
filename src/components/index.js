b6c6130bbd5ac4f873f3b674c70fedb48532d008
PavelG <vargan.pg@gmail.com>
2019-04-02 23:33:45 -0400

    restructure app

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.scss";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      App
+    </BrowserRouter>
+  );
+}
+
+export default App;
