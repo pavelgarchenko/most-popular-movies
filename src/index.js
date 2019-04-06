2c62f0e5280ddb0c51a3fccb60c1636c51a6c5dc
PavelG <vargan.pg@gmail.com>
2019-04-06 17:52:57 -0400

    removed logger and dummy_data

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
 import App from "./components/App";
 import reducer from "./reducers/reducer";
 
 import "./styles.scss";
 
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk);
 const store =  createStore(reducer, middleware);
 
 
