cbd2a0cdb0d7e8a43a88deb34bd4e4a7fda25946
PavelG <vargan.pg@gmail.com>
2019-04-06 16:14:41 -0400

    Update Nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { faArrowLeft, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./Nav.scss";
+
+const Nav = (props) => {
+  const hidden = props.menuIsHidden ? "hidden" : null
+  const arrow = props.arrowBack
+    ? <Link to={props.storedMovieListPath}>
+        <FontAwesomeIcon icon={faArrowLeft} className="arrow"/>
+      </Link>
+    : null
+
+  return (
+    <div className="nav">
+      <div className="title">
+        {arrow}
+        <h1>
+          {props.title}
+        </h1>
+      </div>
+      <div className="dots">
+        <button onClick={props.onToggleMenu}>
+          <FontAwesomeIcon icon={faEllipsisV}/>
+        </button>
+      </div>
+      <div className={"dropdownContent " + hidden}>
+        <Link to="/movie/top_rated" onClick={props.onToggleMenu}>Top Rated</Link>
+        <Link to="/movie/popular" onClick={props.onToggleMenu}>Popular</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Nav;
