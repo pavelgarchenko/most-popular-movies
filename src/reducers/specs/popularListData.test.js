f31d9913371708f48cd499bb829122758da425e5
PavelG <vargan.pg@gmail.com>
2019-04-10 21:55:17 -0400

    update popular and toprated reducer tests

--- src/reducers/specs/popularListData.test.js
+++ src/reducers/specs/popularListData.test.js
@@ -5,11 +5,13 @@ const initialState = {
   fetching: false,
   fetched: false,
   movies: [],
+  page: 1,
+  total_pages: null,
   error: null,
 };
 
 describe('popularListData reducer', () => {
-  
+
   it('should return the initial state', () => {
     expect(popularListData(undefined, {})).toEqual(initialState);
   });
@@ -23,49 +25,62 @@ describe('popularListData reducer', () => {
     )).toEqual(expected);
   });
   
-  it('should handle FETCH_POPULAR_LIST_SUCCESS', () => {
-    const payload = { page: 1, results: [1, 2, 3] }
-    const expected = { 
-      fetching: false, 
-      fetched: true,
-      movies: payload.results,
-      error: null
-    };
+  describe('FETCH_POPULAR_LIST_SUCCESS', () => {
 
-    expect(
-      popularListData(undefined, {
-        type: types.FETCH_POPULAR_LIST_SUCCESS,
-        payload: payload
-      })
-    ).toEqual(expected);
-  });
-  
-  it('should handle FETCH_POPULAR_LIST_SUCCESS and replace existing data ', () => {
-    const existing = { page: 1, results: [1, 2, 3] }
-    const payload = { page: 2, results: [4, 5, 6] }
-    const expected = { 
-      fetching: false, 
-      fetched: true,
-      movies: payload.results,
-      error: null
-    };
-
-    expect(
-      popularListData(existing, {
-        type: types.FETCH_POPULAR_LIST_SUCCESS,
-        payload: payload
-      })
-    ).toEqual(expected);
-  });
-  
-  
+    it('should update state', () => {
+      const payload = { page: 1, results: [1, 2, 3], total_pages: 6 }
+      const expected = { 
+        fetching: false, 
+        fetched: true,
+        movies: payload.results,
+        page: payload.page,
+        total_pages: payload.total_pages,
+        error: null
+      };
+      expect(
+        popularListData(undefined, {
+          type: types.FETCH_POPULAR_LIST_SUCCESS,
+          payload: payload
+        })
+      ).toEqual(expected);
+    });
+      
+    it('Should update page number and add results to existing list', () => {
+      const existing = { 
+        fetching: false, 
+        fetched: true, 
+        page: 1, 
+        total_pages: 6, 
+        movies: [1, 2, 3] 
+      }
+      const payload = { 
+        page: 2, 
+        total_pages: 6, 
+        results: [4, 5, 6] 
+      }
+      const expected = { 
+        fetching: false, 
+        fetched: true,
+        movies: [...existing.movies, ...payload.results],
+        page: payload.page,
+        total_pages: payload.total_pages,
+        error: null
+      };
+      expect(
+        popularListData(existing, {
+          type: types.FETCH_POPULAR_LIST_SUCCESS,
+          payload: payload
+        })
+      ).toEqual(expected);
+    });
+  })
+        
   it('should handle FETCH_POPULAR_LIST_ERROR', () => {
     const payload = { error: "Test error message" }
     const expected = { 
       ...initialState,
       error: payload.error
     };
-
     expect(
       popularListData(undefined, {
         type: types.FETCH_POPULAR_LIST_ERROR,
--- src/reducers/specs/topRatedListData.test.js
+++ src/reducers/specs/topRatedListData.test.js
@@ -1,74 +1,89 @@
-import popularListData from '../popularListData'
+import topRatedListData from '../topRatedListData'
 import * as types from "../../constants/types";
 
 const initialState = {
   fetching: false,
   fetched: false,
   movies: [],
+  page: 1,
+  total_pages: null,
   error: null,
 };
 
-describe('popularListData reducer', () => {
+describe('topRatedListData reducer', () => {
   
   it('should return the initial state', () => {
-    expect(popularListData(undefined, {})).toEqual(initialState);
+    expect(topRatedListData(undefined, {})).toEqual(initialState);
   });
   
-  it('should handle FETCH_POPULAR_LIST_STARTED', () => {
+  it('should handle FETCH_TOP_RATED_LIST_STARTED', () => {
     const expected = { ...initialState, fetching: true };
     expect(
-      popularListData(undefined, {
-        type: types.FETCH_POPULAR_LIST_STARTED
+      topRatedListData(undefined, {
+        type: types.FETCH_TOP_RATED_LIST_STARTED
       }
     )).toEqual(expected);
   });
   
-  it('should handle FETCH_POPULAR_LIST_SUCCESS', () => {
-    const payload = { page: 1, results: [1, 2, 3] }
-    const expected = { 
-      fetching: false, 
-      fetched: true,
-      movies: payload.results,
-      error: null
-    };
-
-    expect(
-      popularListData(undefined, {
-        type: types.FETCH_POPULAR_LIST_SUCCESS,
-        payload: payload
-      })
-    ).toEqual(expected);
-  });
-  
-  it('should handle FETCH_POPULAR_LIST_SUCCESS and replace existing data ', () => {
-    const existing = { page: 1, results: [1, 2, 3] }
-    const payload = { page: 2, results: [4, 5, 6] }
-    const expected = { 
-      fetching: false, 
-      fetched: true,
-      movies: payload.results,
-      error: null
-    };
-
-    expect(
-      popularListData(existing, {
-        type: types.FETCH_POPULAR_LIST_SUCCESS,
-        payload: payload
-      })
-    ).toEqual(expected);
-  });
-  
-  
-  it('should handle FETCH_POPULAR_LIST_ERROR', () => {
+  describe('FETCH_TOP_RATED_LIST_SUCCESS', () => {
+   
+    it('should update state', () => {
+      const payload = { page: 1, results: [1, 2, 3], total_pages: 6 }
+      const expected = { 
+        fetching: false, 
+        fetched: true,
+        movies: payload.results,
+        page: payload.page,
+        total_pages: payload.total_pages,
+        error: null
+      };
+      expect(
+        topRatedListData(undefined, {
+          type: types.FETCH_TOP_RATED_LIST_SUCCESS,
+          payload: payload
+        })
+      ).toEqual(expected);
+    });
+      
+    it('Should update page number and add results to existing list', () => {
+      const existing = { 
+        fetching: false, 
+        fetched: true, 
+        page: 1, 
+        total_pages: 6, 
+        movies: [1, 2, 3] 
+      }
+      const payload = { 
+        page: 2, 
+        total_pages: 6, 
+        results: [4, 5, 6] 
+      }
+      const expected = { 
+        fetching: false, 
+        fetched: true,
+        movies: [...existing.movies, ...payload.results],
+        page: payload.page,
+        total_pages: payload.total_pages,
+        error: null
+      };
+      expect(
+        topRatedListData(existing, {
+          type: types.FETCH_TOP_RATED_LIST_SUCCESS,
+          payload: payload
+        })
+      ).toEqual(expected);
+    });
+  })
+            
+  it('should handle FETCH_TOP_RATED_LIST_ERROR', () => {
     const payload = { error: "Test error message" }
     const expected = { 
       ...initialState,
       error: payload.error
     };
-
     expect(
-      popularListData(undefined, {
-        type: types.FETCH_POPULAR_LIST_ERROR,
+      topRatedListData(undefined, {
+        type: types.FETCH_TOP_RATED_LIST_ERROR,
         error: payload.error
       })
     ).toEqual(expected);
